782316d34e8d10773f352ec558c810b0064ab272
Jonathan Giddy <jgiddy@mesosphere.com>
2019-11-24 15:02:06 +0000

    Complete bootstrap node

--- bootstrap/provision.sh
+++ bootstrap/provision.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-./dcos_generate_config.sh
+cd /vagrant
+
+bash dcos_generate_config.sh
+
+docker run -d -p 80:80 -v $PWD/genconf/serve:/usr/share/nginx/html:ro nginx
+
+genconf/ip-detect > bootstrap-ip
 
