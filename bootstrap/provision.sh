d7eb05c2de36404f1ecd7097538d3785438e8301
Jonathan Giddy <jgiddy@mesosphere.com>
2019-11-29 09:26:59 +0000

    Add ip detect scripts to Open DC/OS

--- bootstrap/provision.sh
+++ bootstrap/provision.sh
@@ -10,6 +10,9 @@ bash dcos_generate_config.sh
 # https://downloads.dcos.io/dcos/testing/pull/6561/windows/dcos_generate_config_win.sh
 bash dcos_generate_config_win.sh
 
+cp ip-detect.ps1 genconf_win
+cp ip-detect-public.ps1 genconf_win
+
 # need extra args to docker run command!
 # cannot make first directory read-only as then we can't mount second
 docker run -d -p 80:80 -v /vagrant/genconf/serve:/usr/share/nginx/html -v /vagrant/genconf_win:/usr/share/nginx/html/2.1/genconf_win:ro nginx
