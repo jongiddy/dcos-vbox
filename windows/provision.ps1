554fa14b2537941448ec7ee6adfc6fdb6487fc7c
Jonathan Giddy <jgiddy@mesosphere.com>
2019-12-19 12:56:49 +0000

    Turn off firewall on Windows node

--- windows/provision.ps1
+++ windows/provision.ps1
@@ -1,10 +1,16 @@
 $ErrorActionPreference = "stop"
 
+# Turn off firewall
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+# Use master as DNS server, to get leader/master.mesos
 $adapters = Get-NetAdapter | Select-Object -ExpandProperty Name
 ForEach($adapter in $adapters) {
     Set-DnsClientServerAddress -InterfaceAlias $adapter -ServerAddresses 172.28.128.21
 }
 
+# Download dcos_install.ps1
 Invoke-WebRequest -UseBasicParsing -Uri http://172.28.128.20:80/windows/prerequisites/dcos_install.ps1 -OutFile C:\Users\vagrant\Downloads\dcos_install.ps1
 
+# Run dcos_install.ps1
 & C:\Users\vagrant\Downloads\dcos_install.ps1 http://172.28.128.20:80 172.28.128.21
