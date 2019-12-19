860379fe8a0f831722ba8f0e8bd17ef6015f72ee
Jonathan Giddy <jgiddy@mesosphere.com>
2019-12-19 12:04:33 +0000

    Set DNS for Windows node to point to master

--- windows/provision.ps1
+++ windows/provision.ps1
@@ -1,3 +1,10 @@
+$ErrorActionPreference = "stop"
+
+$adapters = Get-NetAdapter | Select-Object -ExpandProperty Name
+ForEach($adapter in $adapters) {
+    Set-DnsClientServerAddress -InterfaceAlias $adapter -ServerAddresses 172.28.128.21
+}
+
 Invoke-WebRequest -UseBasicParsing -Uri http://172.28.128.20:80/windows/prerequisites/dcos_install.ps1 -OutFile C:\Users\vagrant\Downloads\dcos_install.ps1
 
 & C:\Users\vagrant\Downloads\dcos_install.ps1 http://172.28.128.20:80 172.28.128.21
