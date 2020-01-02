9b636e4e3050561ea298c58003ffecd910392888
Jonathan Giddy <jgiddy@mesosphere.com>
2020-01-02 09:50:20 +0000

    Add CLI test for Windows node

---
+++ cli-test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e -x
+
+if [ ! -f dcos ]
+then
+	curl https://downloads.dcos.io/binaries/cli/linux/x86-64/dcos-1.13/dcos -o dcos
+fi
+
+chmod +x dcos
+
+./dcos cluster setup --username=admin --password=admin http://172.28.128.21
+
+./dcos cluster list
+
+./dcos node list
+
+# Get a Windows diagnostics bundle
+rm -f bundle.zip
+bundle_id=$$
+curl -XPUT -H "Authorization: token=$(dcos config show core.dcos_acs_token)" http://172.28.128.31:61001/system/health/v1/node/diagnostics/${bundle_id}
+status=$(curl -H "Authorization: token=$(dcos config show core.dcos_acs_token)" http://172.28.128.31:61001/system/health/v1/node/diagnostics/${bundle_id} | jq '.status')
+while [ $status != '"Done"' ]
+do
+	echo $status
+	sleep 5
+	status=$(curl -H "Authorization: token=$(dcos config show core.dcos_acs_token)" http://172.28.128.31:61001/system/health/v1/node/diagnostics/${bundle_id} | jq '.status')
+done
+curl --output bundle.zip -H "Authorization: token=$(dcos config show core.dcos_acs_token)" http://172.28.128.31:61001/system/health/v1/node/diagnostics/${bundle_id}/file
+
+# Getting bundle does not complete on master
+# diagnostics_id=$(./dcos diagnostics create)
+# status=$(./dcos diagnostics list --json | jq 'map(select(.id=="'"$diagnostics_id"'")) | .[0].status')
+# while [ $status != '"Done"' ]
+# do
+# 	echo $status
+# 	sleep 5
+# 	status=$(./dcos diagnostics list --json | jq 'map(select(.id=="'"$diagnostics_id"'")) | .[0].status')
+# done
