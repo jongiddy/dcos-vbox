85515731d4576d77b6b1af91ba71c0407290012d
Jonathan Giddy <jgiddy@mesosphere.com>
2020-01-24 14:33:11 +0000

    Add test for multiple dcos-diagnostics endpoints

--- cli-test.sh
+++ cli-test.sh
@@ -26,14 +26,18 @@ do
 	sleep 5
 	status=$(curl -H "Authorization: token=$(dcos config show core.dcos_acs_token)" http://172.28.128.31:61001/system/health/v1/node/diagnostics/${bundle_id} | jq '.status')
 done
-curl --output bundle.zip -H "Authorization: token=$(dcos config show core.dcos_acs_token)" http://172.28.128.31:61001/system/health/v1/node/diagnostics/${bundle_id}/file
+curl --output bundle-61001.zip -H "Authorization: token=$(dcos config show core.dcos_acs_token)" http://172.28.128.31:61001/system/health/v1/node/diagnostics/${bundle_id}/file
+
+curl --insecure --output bundle-61002.zip -H "Authorization: token=$(dcos config show core.dcos_acs_token)" https://172.28.128.31:61002/system/health/v1/node/diagnostics/${bundle_id}/file
+
+curl --output bundle-1050.zip -H "Authorization: token=$(dcos config show core.dcos_acs_token)" http://172.28.128.31:1050/system/health/v1/node/diagnostics/${bundle_id}/file
 
 # Getting bundle does not complete on master
-# diagnostics_id=$(./dcos diagnostics create)
-# status=$(./dcos diagnostics list --json | jq 'map(select(.id=="'"$diagnostics_id"'")) | .[0].status')
-# while [ $status != '"Done"' ]
-# do
-# 	echo $status
-# 	sleep 5
-# 	status=$(./dcos diagnostics list --json | jq 'map(select(.id=="'"$diagnostics_id"'")) | .[0].status')
-# done
+diagnostics_id=$(./dcos diagnostics create)
+status=$(./dcos diagnostics list --json | jq 'map(select(.id=="'"$diagnostics_id"'")) | .[0].status')
+while [ $status != '"Done"' ]
+do
+	echo $status
+	sleep 5
+	status=$(./dcos diagnostics list --json | jq 'map(select(.id=="'"$diagnostics_id"'")) | .[0].status')
+done
