71f3c3b14ef52c2f4bf4b3bf3c5fba20137030ca
Jonathan Giddy <jgiddy@mesosphere.com>
2020-03-24 18:20:53 +0000

    Copy Windows config files into top-level directory

--- bootstrap-ee/provision.sh
+++ bootstrap-ee/provision.sh
@@ -11,13 +11,12 @@ fi
 mkdir genconf
 
 cp config.yaml fault-domain-detect ip-detect license.txt genconf/
+cp ip-detect.ps1 ip-detect-public.ps1 fault-domain-detect-win.ps1 genconf/
 
 bash dcos_generate_config.ee.sh
 
 bash dcos_generate_config_win.ee.sh
 
 rm dcos-genconf.*.tar
-
-cp ip-detect.ps1 ip-detect-public.ps1 fault-domain-detect-win.ps1 genconf/serve/windows/
 
 docker run -d -p 80:80 -v /vagrant/genconf/serve:/usr/share/nginx/html:ro nginx
--- bootstrap/provision.sh
+++ bootstrap/provision.sh
@@ -11,13 +11,12 @@ fi
 mkdir genconf
 
 cp config.yaml fault-domain-detect ip-detect genconf/
+cp ip-detect.ps1 ip-detect-public.ps1 fault-domain-detect-win.ps1 genconf/
 
 bash dcos_generate_config.sh
 
 bash dcos_generate_config_win.sh
 
 rm dcos-genconf.*.tar
-
-cp ip-detect.ps1 ip-detect-public.ps1 fault-domain-detect-win.ps1 genconf/serve/windows/
 
 docker run -d -p 80:80 -v /vagrant/genconf/serve:/usr/share/nginx/html:ro nginx
