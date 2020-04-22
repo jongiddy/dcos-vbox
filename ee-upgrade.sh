65e0466d294bab7e5275d0ea7a70f199678b5ddc
Jonathan Giddy <jgiddy@mesosphere.com>
2020-04-22 16:01:33 +0100

    Upgrade using the correct version

--- ee-upgrade.sh
+++ ee-upgrade.sh
@@ -11,16 +11,18 @@ case "${security}" in
 		;;
 esac
 
+version=$(curl http://172.28.128.21/dcos-metadata/dcos-version.json | jq --join-output .version)
+
 cp bootstrap-ee/config-${security}.yaml bootstrap-ee/config.yaml
 
-(cd bootstrap-ee; bash ./dcos_generate_config.ee.sh --generate-node-upgrade-script 2.1.0-beta2-dev > upgrade.out)
+(cd bootstrap-ee; bash ./dcos_generate_config.ee.sh --generate-node-upgrade-script ${version} > upgrade.out)
 cat bootstrap-ee/upgrade.out
 url=$(grep 'Node upgrade script URL:' bootstrap-ee/upgrade.out | sed -e 's#Node upgrade script URL: \(http.\+/dcos_node_upgrade.sh\)#\1#')
 
 (cd master; vagrant ssh -c "curl $url --output dcos_upgrade.sh")
 (cd master; vagrant ssh -c "sudo bash ./dcos_upgrade.sh")
 
-(cd bootstrap-ee; bash ./dcos_generate_config.ee.sh --generate-node-upgrade-win-script 2.1.0-beta2-dev > upgrade.out)
+(cd bootstrap-ee; bash ./dcos_generate_config.ee.sh --generate-node-upgrade-win-script ${version} > upgrade.out)
 cat bootstrap-ee/upgrade.out
 url=$(grep 'Windows agent upgrade script URL:' bootstrap-ee/upgrade.out | sed -e 's#Windows agent upgrade script URL: \(http.\+/dcos_node_upgrade.ps1\)#\1#')
 
--- open-upgrade.sh
+++ open-upgrade.sh
@@ -2,14 +2,16 @@
 
 set -e -x
 
-(cd bootstrap; bash ./dcos_generate_config.sh --generate-node-upgrade-script 2.1.0-beta2-dev > upgrade.out)
+version=$(curl http://172.28.128.21/dcos-metadata/dcos-version.json | jq --join-output .version)
+
+(cd bootstrap; bash ./dcos_generate_config.sh --generate-node-upgrade-script ${version} > upgrade.out)
 cat bootstrap/upgrade.out
 url=$(grep 'Node upgrade script URL:' bootstrap/upgrade.out | sed -e 's#Node upgrade script URL: \(http.\+/dcos_node_upgrade.sh\)#\1#')
 
 (cd master; vagrant ssh -c "curl $url --output dcos_upgrade.sh")
 (cd master; vagrant ssh -c "sudo bash ./dcos_upgrade.sh")
 
-(cd bootstrap; bash ./dcos_generate_config.sh --generate-node-upgrade-win-script 2.1.0-beta2-dev > upgrade.out)
+(cd bootstrap; bash ./dcos_generate_config.sh --generate-node-upgrade-win-script ${version} > upgrade.out)
 cat bootstrap/upgrade.out
 url=$(grep 'Windows agent upgrade script URL:' bootstrap/upgrade.out | sed -e 's#Windows agent upgrade script URL: \(http.\+/dcos_node_upgrade.ps1\)#\1#')
 
